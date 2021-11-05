ea8fe89546fc787c49e0870958eea664a4aecdc6
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-11-06 02:51:47 +0530

    Modified Json file

--- app.js
+++ app.js
@@ -4,23 +4,42 @@ patient.config(function ($routeProvider) {
 
 });
 
-patient.controller('patientController', function ($scope, $http) {
-    $http({
-        method: 'GET',
-        url: '/Data/Doctors.json'
-    }).then(function (data) {
-        console.log('Hi');
-        // $scope.doctor = data;
-    }, function (error) {
-        console.log('Error');
-    });
+patient.controller('patientController', function ($scope) {
+    $scope.doctor = [
+        {
+            "name": "Dr. Aman Jain",
+            "spec": "Cardiologist",
+            "fee": 200,
+            "thumb": "/Pictures/abcd.jpg"
+        },
+        {
+            "name": "Dr. Shruti Jha",
+            "spec": "Opthalmologist",
+            "fee": 200,
+            "thumb": "/Pictures/pqrs.jpeg"
+        },
+        {
+            "name": "Dr. Kamal Chhajer",
+            "spec": "Child Specialist",
+            "fee": 200,
+            "thumb": "/Pictures/xyz.jpg"
+        },
+        {
+            "name": "Dr. Shivram Goenka",
+            "spec": "Neuro-Surgeon",
+            "fee": 200,
+            "thumb": "/Pictures/abcd.jpg"
+        }
+    ]
 })
-//     $http.get("/Views/Doctors.json").then(successCallBack, errorCallBack);
-//     function successCallBack(data) {
+// patient.controller('patientBController', function ($scope, $http) {
+//     $http({
+//         method: 'GET',
+//         url: '/Data/Doctors.json'
+//     }).then(function (data) {
+//         console.log(data);
 //         $scope.doctor = data;
-//     }
-//     function errorCallBack(response) {
-//         console.log(response);
-//     }
-
-// });
+//     }, function (error) {
+//         console.log('Error');
+//     });
+// })
