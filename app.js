10937955028b6bd7b1f6ecb44445049c641aea6e
Shraddha Suman <shraddhasuman2526@gmail.com>
2021-11-05 23:04:56 +0530

    modified app.js

--- app.js
+++ app.js
@@ -4,24 +4,3 @@ patient.config(function ($routeProvider) {
 
 });
 
-patient.controller('patientController', function ($scope) {
-    $scope.doctor = [{
-        name: "Dr. Aman Jain",
-        spec: "heart",
-        fee: 200,
-        thumb: "Pictures/abcd.jpg",
-    },
-    {
-        name: "Dr. Shruti Jha",
-        spec: "brain",
-        fee: 200,
-        thumb: "Pictures/pqrs.jpeg",
-    },
-    {
-        name: "Dr. Kamal Chhajer",
-        spec: "teeth",
-        fee: 200,
-        thumb: "Pictures/xyz.jpg",
-    }
-    ]
-})
