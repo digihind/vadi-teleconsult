4cb5213dec7e9b7b8043cc6bbbb921d8295308ed
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-11-05 18:36:58 +0530

    Added Sample images

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
+var patient = angular.module('patient', ['ngRoute']);
 
-var patient = angular.module('patient',[]);
-patient.controller('patientController',function($scope){
-    $scope.doctor=[{
-        name:"abcd",
-        spec:"heart"
+patient.config(function ($routeProvider) {
+
+});
+
+patient.controller('patientController', function ($scope) {
+    $scope.doctor = [{
+        name: "abcd",
+        spec: "heart",
+        fee: 200,
+        thumb: "Pictures/abcd.jpg",
     },
     {
-        name:"pqrs",
-        spec:"brain"
+        name: "pqrs",
+        spec: "brain",
+        fee: 200,
+        thumb: "Pictures/pqrs.jpeg",
     },
     {
-        name:"xyz",
-        spec:"teeth"
+        name: "xyz",
+        spec: "teeth",
+        fee: 200,
+        thumb: "Pictures/xyz.jpg",
     }
-]
+    ]
 })
