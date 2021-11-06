0a3fc0c5cee491060215801dac4f818e60dabc41
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-11-06 18:42:48 +0530

    git add .

--- app.js
+++ app.js
@@ -29,9 +29,33 @@ patient.controller('patientController', function ($scope) {
             "spec": "Neuro-Surgeon",
             "fee": 200,
             "thumb": "/Pictures/abcd.jpg"
-        }
+        },
+        {
+            "name": "Dr. Kamal Chhajer",
+            "spec": "Child Specialist",
+            "fee": 200,
+            "thumb": "/Pictures/xyz.jpg"
+        },
     ]
 })
+$(document).ready(function () {
+    var cnt = 0;
+    $(".chat").click(function () {
+        cnt++;
+        console.log('CLK');
+        $(".frm").toggle();
+        console.log(cnt);
+        if (cnt % 2 !== 0) {
+            console.log("1st");
+            $("#details").attr("class", "blur");
+        }
+        else {
+            console.log('2nd');
+            $("#details").attr("class", "none");
+        }
+    });
+});
+
 // patient.controller('patientBController', function ($scope, $http) {
 //     $http({
 //         method: 'GET',
