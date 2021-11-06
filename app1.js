e2e74883b9fd70c90357d6422ec078e2795e5576
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-11-07 02:54:32 +0530

    Added chatscreen for doctor

--- app1.js
+++ app1.js
@@ -1,35 +1,49 @@
 
 var doctor = angular.module('doctor', ['ngRoute']);
 
-doctor.config(function ($routeProvider) {
-
-});
-
 doctor.controller('doctorController', function ($scope) {
     $scope.patient = [
         {
             "name": "Harshit Arora",
             "age": 24,
-            "phone_no":9780123234,
-            "issue":"Teeth Pain"
+            "phone_no": 9780123234,
+            "issue": "Teeth Pain"
         },
         {
             "name": "Ishita Dutta",
             "age": 34,
-            "phone_no":9780343234,
-            "issue":"Severe back pain"
+            "phone_no": 9780343234,
+            "issue": "Severe back pain"
         },
         {
             "name": "Aafiya Ahmed",
             "age": 37,
-            "phone_no":7980123234,
-            "issue":"Stomach ache"
+            "phone_no": 7980123234,
+            "issue": "Stomach ache"
         },
         {
             "name": "Shipra Sharma",
             "age": 22,
-            "phone_no":9780123278,
-            "issue":"Headache cough and cold"
+            "phone_no": 9780123278,
+            "issue": "Headache cough and cold"
         }
     ]
 })
+$(document).ready(function () {
+    var cnt = 0;
+    $(".chat").click(function () {
+        cnt++;
+        console.log('CLK');
+        $(".frm").toggle();
+        console.log($("span").html());
+        $("#details").attr("class", "blur");
+
+
+    });
+    $('.header-area span').click(function () {
+        console.log('clicked');
+        console.log($(".frm").html());
+        $(".frm").toggle();
+        $("#details").attr("class", "none");
+    })
+});
