07a3c8c363a8ecd8401e5fa05233fbe84760c8a6
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-11-15 04:10:06 +0530

    Added animations to Login/Signup Screen

--- login.js
+++ login.js
@@ -1,12 +1,14 @@
 
-// var sign_in_btn = document.querySelector('#sign-in-button');
-// var sign_up_btn = document.querySelector('#sign-up-button');
-// var container = document.querySelector('.container');
-// // console.log(sign_in_btn.innerHTML);
-// sign_up_btn.addEventListener("click", function(){
-//     container.classList.add('sign-up-mode');
-// });
+const sign_in_btn = document.querySelector('#sign-in-button');
+const sign_up_btn = document.querySelector('#sign-up-button');
+const container = document.querySelector('.container');
 
-// sign_in_btn.addEventListener("click", function(){
-//     container.classList.remove('sign-up-mode');
-// });
+sign_up_btn.addEventListener("click", function () {
+    container.classList.add('sign-up-mode');
+    container.classList.remove('sign-in-mode');
+});
+
+sign_in_btn.addEventListener("click", function () {
+    container.classList.remove('sign-up-mode');
+    container.classList.add('sign-in-mode');
+});
