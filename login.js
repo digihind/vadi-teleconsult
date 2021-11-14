26ef3a1da65871ba3fe0b20f5b84cab858d27e2b
Udith Shyamsukha <udithshyamsukha@gmail.com>
2021-11-15 01:04:59 +0530

    Changed login page

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 
-var sign_in_btn = document.querySelector('#sign-in-button');
-var sign_up_btn = document.querySelector('#sign-up-button');
-var container = document.querySelector('.container');
-// console.log(sign_in_btn.innerHTML);
-sign_up_btn.addEventListener("click", function(){
-    container.classList.add('sign-up-mode');
-});
+// var sign_in_btn = document.querySelector('#sign-in-button');
+// var sign_up_btn = document.querySelector('#sign-up-button');
+// var container = document.querySelector('.container');
+// // console.log(sign_in_btn.innerHTML);
+// sign_up_btn.addEventListener("click", function(){
+//     container.classList.add('sign-up-mode');
+// });
 
-sign_in_btn.addEventListener("click", function(){
-    container.classList.remove('sign-up-mode');
-});
+// sign_in_btn.addEventListener("click", function(){
+//     container.classList.remove('sign-up-mode');
+// });
